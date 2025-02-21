c1ffbd2e5bca00f5585b473fa8913a63b50ee8c9
supernova <supernova@onlyevm.com>
2025-02-21 18:08:44 +0530

    working script

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use alloy::primitives::{address, Address, U256};
 use alloy::primitives::{Bytes, FixedBytes};
 use alloy::providers::WalletProvider;
+use eigen_types::operator::Operator;
 use eigen_utils::core::delegationmanager::DelegationManager;
 use eigen_utils::middleware::registrycoordinator::RegistryCoordinator;
 use eigen_utils::middleware::stakeregistry::StakeRegistry;
@@ -13,14 +14,12 @@ use eigensdk::common::{get_provider, get_signer};
 use eigensdk::crypto_bls::BlsKeyPair;
 use eigensdk::logging::get_test_logger;
 use eigensdk::testing_utils::m2_holesky_constants::{
-    AVS_DIRECTORY_ADDRESS, DELEGATION_MANAGER_ADDRESS,
-     REWARDS_COORDINATOR, STRATEGY_MANAGER_ADDRESS,
+    AVS_DIRECTORY_ADDRESS, DELEGATION_MANAGER_ADDRESS, REWARDS_COORDINATOR,
+    STRATEGY_MANAGER_ADDRESS,
 };
-use eigen_types::operator::Operator;
 use eyre::Result;
 use lazy_static::lazy_static;
 use std::time::{SystemTime, UNIX_EPOCH};
-
 
 lazy_static! {
     /// 1 day
@@ -28,23 +27,22 @@ lazy_static! {
 }
 #[tokio::main]
 async fn main() -> Result<()> {
-   
     let holesky_provider = "https://ethereum-holesky-rpc.publicnode.com";
     let pvt_key = "68adc67158ef2a42183f797c9f88e8d8dd9dfe0214f9e781f97f4d163130db0e";
     let OPACITY_REGISTRY_COORDINATOR = address!("3e43AA225b5cB026C5E8a53f62572b10D526a50B");
     let signer = get_signer(&pvt_key, &holesky_provider);
     let test_logger = get_test_logger();
-    let contract_registry_coordinator = RegistryCoordinator::new(OPACITY_REGISTRY_COORDINATOR, get_provider(&holesky_provider));
-    let s = contract_registry_coordinator.serviceManager().call().await?._0;
-    let y =contract_registry_coordinator.blsApkRegistry().call().await?._0;
-    let u = contract_registry_coordinator.stakeRegistry().call().await?._0;
-    let contract_stake_registry = StakeRegistry::new(u, get_provider(&holesky_provider));
-    let i = contract_stake_registry.delegation().call().await?._0;
-    let contract_service_manager = MockAvsServiceManager::new(s, get_provider(&holesky_provider));
-    let d = contract_service_manager.avsDirectory().call().await?._0;
-    let dm = DelegationManager::new(i, get_provider(&holesky_provider));
-    let q = dm.slasher().call().await?._0;
-    dbg!("ss");
+    // let contract_registry_coordinator = RegistryCoordinator::new(OPACITY_REGISTRY_COORDINATOR, get_provider(&holesky_provider));
+    // let s = contract_registry_coordinator.serviceManager().call().await?._0;
+    // let y =contract_registry_coordinator.blsApkRegistry().call().await?._0;
+    // let u = contract_registry_coordinator.stakeRegistry().call().await?._0;
+    // let contract_stake_registry = StakeRegistry::new(u, get_provider(&holesky_provider));
+    // let i = contract_stake_registry.delegation().call().await?._0;
+    // let contract_service_manager = MockAvsServiceManager::new(s, get_provider(&holesky_provider));
+    // let d = contract_service_manager.avsDirectory().call().await?._0;
+    // let dm = DelegationManager::new(i, get_provider(&holesky_provider));
+    // let q = dm.slasher().call().await?._0;
+    // dbg!("ss");
 
     let avs_registry_writer = AvsRegistryChainWriter::build_avs_registry_chain_writer(
         test_logger.clone(),
@@ -76,39 +74,41 @@ async fn main() -> Result<()> {
     } else {
         println!("System time seems to be before the UNIX epoch.");
     }
-    let quorum_nums = Bytes::from([0x01]);
+    let quorum_nums = Bytes::from([0x00]);
 
     // A new ElChainReader instance
     let el_chain_reader = ELChainReader::new(
         get_test_logger().clone(),
-        Address::ZERO,
+        None,
         DELEGATION_MANAGER_ADDRESS,
         REWARDS_COORDINATOR,
         AVS_DIRECTORY_ADDRESS,
+        None,
         holesky_provider.to_string(),
     );
-
 
     // A new ElChainWriter instance
     let el_writer = ELChainWriter::new(
         STRATEGY_MANAGER_ADDRESS,
         REWARDS_COORDINATOR,
+        None,
+        None,
+        OPACITY_REGISTRY_COORDINATOR,
         el_chain_reader,
         holesky_provider.to_string(),
-        pvt_key.to_string()
+        pvt_key.to_string(),
     );
-
 
     let operator_details = Operator {
         address: signer.default_signer_address(),
-        delegation_approver_address: signer.default_signer_address(),
-        metadata_url: Some("eigensdk-rs".to_string()),
-        earnings_receiver_address:signer.default_signer_address(),
-        staker_opt_out_window_blocks: 10,
+        delegation_approver_address: Address::ZERO,
+        metadata_url: "opacity-eigensdk-rs".to_string(),
+        _deprecated_earnings_receiver_address: Some(signer.default_signer_address()),
+        staker_opt_out_window_blocks: Some(10),
+        allocation_delay: Some(100),
     };
     // Register the address as operator in delegation manager
     let _s = el_writer.register_as_operator(operator_details).await?;
-    dbg!(_s);
 
     // Register the operator in registry coordinator
     let tx = avs_registry_writer
@@ -122,5 +122,4 @@ async fn main() -> Result<()> {
         .await?;
     dbg!(tx);
     Ok(())
-
 }
